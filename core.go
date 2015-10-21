c94d0a1a82808bccd2d3798358f145131b1a8790
sadlil <sadlil@thetigerworks.com>
2015-10-21 13:19:44 +0600

    Test Added.

--- core.go
+++ core.go
@@ -20,7 +20,7 @@ func add(event string, task interface{}) error {
 }
 
 
-func invoke(event string, params ...interface{}) (result []reflect.Value, err error) {
+func invoke(event string, params ...interface{}) ([]reflect.Value, error) {
 	f := reflect.ValueOf(functionMap[event])
 	if len(params) != f.Type().NumIn() {
 		return nil, errors.New("Parameter Mismatched")
@@ -29,6 +29,37 @@ func invoke(event string, params ...interface{}) (result []reflect.Value, err er
 	for k, param := range params {
 		in[k] = reflect.ValueOf(param)
 	}
-	result = f.Call(in)
+	result := f.Call(in)
 	return result, nil
+}
+
+
+func clear(event string) error {
+	if _, ok := functionMap[event]; !ok {
+		return errors.New("Event Not Defined")
+	}
+	delete(functionMap, event)
+	return nil
+}
+
+func deleteAll() error {
+	functionMap = make(map[string]interface{})
+	return nil
+}
+
+func eventList() []string {
+	events := make([]string, 0)
+	for k := range functionMap {
+		events = append(events, k)
+	}
+	return events
+}
+
+func eventCount() int {
+	return len(functionMap)
+}
+
+func hasEvent(event string) bool {
+	_, ok := functionMap[event]
+	return ok
 }
--- trigger.go
+++ trigger.go
@@ -2,10 +2,30 @@ package trigger
 
 import "reflect"
 
-func On(event string, task interface{}) {
-	add(event, task)
+func On(event string, task interface{}) error {
+	return add(event, task)
 }
 
 func Fire(event string, params ...interface{}) (result []reflect.Value, err error) {
 	return invoke(event, params...)
+}
+
+func Clear(event string) error {
+	return clear(event)
+}
+
+func ClearEvents() error {
+	return deleteAll()
+}
+
+func HasEvent(event string) bool {
+	return hasEvent(event)
+}
+
+func Events() []string {
+	return eventList()
+}
+
+func EventCount() int {
+	return eventCount()
 }
